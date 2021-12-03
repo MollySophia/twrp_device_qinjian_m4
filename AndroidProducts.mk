e5c2589b9f1ade527ff3dacb24a895d6bc689d07
Molly Sophia <mollysophia379@gmail.com>
2021-12-04 04:48:03 +0800

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m4_carbine.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m4_carbine-user \
+    omni_m4_carbine-userdebug \
+    omni_m4_carbine-eng
---
+++ omni_m4_carbine.mk
@@ -0,0 +1,9 @@
+# Inherit from m4_carbine device
+$(call inherit-product, device/szjy/m4_carbine/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m4_carbine
+PRODUCT_NAME := omni_m4_carbine
+PRODUCT_BRAND := Q-Love
+PRODUCT_MODEL := Q8312A
+PRODUCT_MANUFACTURER := szjy
